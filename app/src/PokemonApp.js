f7e984cf8ee07fbf99bc9071941d10a8b87c4c16
Ira Nefedova <iraonegina@yandex.ru>
2017-05-22 22:48:14 +0300

    berries list

--- app/src/PokemonApp.js
+++ app/src/PokemonApp.js
@@ -11,6 +11,10 @@ module('PokemonApp')
             templateUrl: 'src/PokemonList/PokemonList.html',
             controller: 'PokemonListCtrl'
         }).
+        when('/berries', {
+            templateUrl: 'src/BerriesList/BerriesList.html',
+            controller: 'BerriesListCtrl'
+        }).
         when('/pokemons/:pokemonId', {
             templateUrl: 'src/PokemonDetail/PokemonDetail.html',
             controller: 'PokemonDetailCtrl'
@@ -31,8 +35,8 @@ module('PokemonApp')
 
 .config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.headers.common = {
-        "application-id": "4B730C92-F81E-236B-FFF0-6651FE882800",
-        "secret-key": "CB6DE86C-6069-86C4-FF1C-9049D5AC9400"
+        "application-id": "BBE2BD1F-9306-F389-FFCE-0D7310E74300",
+        "secret-key": "21A60E66-E921-977A-FF7D-AF3F75E3B400"
     };
 
 }]);
